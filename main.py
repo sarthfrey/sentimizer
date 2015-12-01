1ee78bc1867f315f729ee39c047cf58175b9670f
sarthfrey <sarth.frey@gmail.com>
2015-11-30 19:50:05 -0500

    Added final answer report

--- main.py
+++ main.py
@@ -13,32 +13,35 @@ from google.appengine.api import urlfetch
 def buildSentReq(text, api_key):
     return 'https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=' + text + '&apikey=' + api_key
   
-def getScore(text):
-    url = buildSentReq(text, '944a963d-b63c-4d65-a562-d9507ca49571')
-    result = urlfetch.fetch(url)
-    if result.status_code == 200:
-        jsonSent = json.loads(result.content)
-        return jsonSent['aggregate']['score']
+def getData(text):
+	text = text.replace(" ","+")
+	url = buildSentReq(text, '944a963d-b63c-4d65-a562-d9507ca49571')
+	result = urlfetch.fetch(url)
+	if result.status_code == 200:
+		jsonSent = json.loads(result.content)
+		score = jsonSent['aggregate']['score']
+		answer = jsonSent['aggregate']['sentiment']
+		return [score, answer]
 #END UNMODULATED CODE
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/sentiment/', methods=['POST'])
 def sentiment():
 	sentiment_text = request.form['sentiment']
-	score = getScore(sentiment_text)	#NEEDS TO BE MODULATED
-	return render_template('index.html', sentiment = "Text: " + sentiment_text, score = "Score: " + str(score))
+	score, answer = getData(sentiment_text)	#NEEDS TO BE MODULATED
+	return render_template('index.html', sentiment = "Text: " + sentiment_text, score = "Score: " + str(score), answer = "Conclusion: " + answer)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+	"""Return a custom 404 error."""
+	return 'Sorry, Nothing at this URL.', 404
 
 
 @app.errorhandler(500)
 def application_error(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+	"""Return a custom 500 error."""
+	return 'Sorry, unexpected error: {}'.format(e), 500
--- sentiment.py
+++ sentiment.py
@@ -4,9 +4,9 @@ Created on Sat Oct 31 18:18:54 2015
 
 @author: prcobol
 """
-#import urllib3
+import urllib3
 import json
-from google.appengine.api import urlfetch
+#from google.appengine.api import urlfetch
 #from havenondemand.hodindex import HODClient
 
 
@@ -20,16 +20,17 @@ def buildCallReq(audio_url, api_key):
 def buildCalJobID(jobID, api_key):
     return 'https://api.havenondemand.com/1/job/result/' + jobID +'?apikey=' + api_key
 
-#def getScore(text):
-#    http = urllib3.PoolManager()
-#    reqSent = http.request('POST', 
-#    jsonSent = json.loads(reqSent.data)
-#    return jsonSent['aggregate']['score']
-    
-    
 def getScore(text):
-    url = buildSentReq(text, '944a963d-b63c-4d65-a562-d9507ca49571')
-    result = urlfetch.fetch(url)
-    if result.status_code == 200:
-        jsonSent = json.loads(result.content)
-        return jsonSent['aggregate']['score']
+    text = text.replace(" ","+")
+    http = urllib3.PoolManager()
+    reqSent = http.request('POST', buildSentReq(text, '944a963d-b63c-4d65-a562-d9507ca49571'))
+    jsonSent = json.loads(reqSent.data)
+    return jsonSent['aggregate']['score']
+
+    
+#def getScore(text):
+#    url = buildSentReq(text, '944a963d-b63c-4d65-a562-d9507ca49571')
+#    result = urlfetch.fetch(url)
+#    if result.status_code == 200:
+#        jsonSent = json.loads(result.content)
+#        return jsonSent['aggregate']['score']
